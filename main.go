9c0f8257978c0430542703e23f99e07d765594de
Daniel Schauenberg <d@unwiredcouch.com>
2014-09-10 11:18:42 +0200

    formatting and better error handling

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"os"
 	"fmt"
-	"net/mail"
 	"github.com/jhillyerd/go.enmime"
+	"net/mail"
+	"os"
 )
 
 var Debug bool
 
 func main() {
-  m, _ := mail.ReadMessage(os.Stdin)
+	m, _ := mail.ReadMessage(os.Stdin)
 
-  body, err := enmime.ParseMIMEBody(m)
-  if err != nil {
-    fmt.Println("Error parsing MIME body.")
-  }
-  fmt.Println(m.Header.Get("Subject"))
-  fmt.Printf("%v", body.Text)
+	body, err := enmime.ParseMIMEBody(m)
+	if err != nil {
+		fmt.Println("Error parsing MIME body.")
+	} else {
+		fmt.Println(m.Header.Get("Subject"))
+		fmt.Printf("%s", body.Text)
+	}
 }
